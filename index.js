5ec3083e90bd83a002bb0f4a247b04d4db7b2f3c
Franco <gutierrezfranco0@gmail.com>
2022-10-26 11:35:47 -0300

    com

---
+++ index.js
@@ -0,0 +1,63 @@
+const { json } = require('express');
+const express = require('express')
+const app = express()
+const port = process.env.port || 3000;
+let message = JSON.parse('{"message":"Hello json"}')
+const data = {
+    "name": "Franco Gutierrez",
+    "acerca": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, ex iusto, ea voluptatum obcaecati nesciunt ducimus quos, id quam vero aliquid! Nobis nostrum architecto nemo obcaecati beatae odit labore quibusdam.",
+    "proyectos": [{
+      "pname": "Apple Page",
+      "img":"https://i.imgur.com/VoVygxA.png",
+      "pdesc": "Una landing page con tematica de Apple creada 100% en codigo nativo HTML y CSS",
+      "url": "https://gutierrezfranco.github.io/apple/",
+      "urlrepo": "https://github.com/GutierrezFranco/apple"
+    },{
+        "pname": "CRUD Alquileres",
+        "img":"https://i.imgur.com/a6GXAyV.png",
+        "pdesc": "CRUD para tener a mano los alquileres que me interesaron, usando HTML, CSS, PHP y MySQL",
+        "url": "https://gutierrezfranco.github.io/apple/",
+        "urlrepo": "https://github.com/GutierrezFranco/apple"
+      },{
+        "pname": "Calculadora de Intereses",
+        "img": "https://i.imgur.com/iz8q9YV.png",
+        "pdesc": "App para calcular cuotas, creada con HTML, CSS y JS",
+        "url": "http://francogutierrez.com.ar/",
+        "urlrepo": "#"
+      }],
+      "skills": [{
+        "tecnologia": "HTML",
+        "porcentaje": "90%"
+      },{
+        "tecnologia": "CSS",
+        "porcentaje": "80%"
+      },{
+        "tecnologia": "JS",
+        "porcentaje": "70%"
+      },{
+        "tecnologia": "MySQL",
+        "porcentaje": "70%"
+      },{
+        "tecnologia": "PHP",
+        "porcentaje": "60%"
+      }
+      ]
+      
+  };
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+  console.log(data.name)
+})
+
+app.get('/json', (req, res) => {
+    res.json(data);
+})
+
+
+console.log("Mandando mecha...");
